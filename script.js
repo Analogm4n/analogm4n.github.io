9ff61d92826dc11749faa75b8a72f7f5ee07e3b8
Jp <160363802+Analogm4n@users.noreply.github.com>
2024-11-02 15:51:59 +0100

    Add files via upload
    js update

---
+++ script.js
@@ -0,0 +1,30 @@
+const pet = document.getElementById('pet');
+
+const petGifRight = 'cat_right_sneak1_9fps.gif';  
+const petGifLeft = 'cat_left_sneak1_9fps.gif';  
+
+let positionX = 0;
+let direction = 1; // 1: derecha, -1: izquierda
+
+// Establece los límites
+const leftLimit = 0; // Límite izquierdo (0 píxeles desde el borde izquierdo)
+const rightLimit = window.innerWidth - pet.clientWidth ; // Límite derecho (0 píxeles desde el borde derecho)
+
+// Función para mover de un lado a otro
+function movePet() {
+    // Cambia la dirección si alcanza un borde de la pantalla
+    if (positionX >= rightLimit) {
+        direction = -1; 
+        pet.src = petGifLeft; 
+    } else if (positionX <= leftLimit) {
+        direction = 1; 
+        pet.src = petGifRight; 
+    }
+
+    // Actualiza la posición en X
+    positionX += 5 * direction;
+    pet.style.transform = `translateX(${positionX}px)`;
+}
+
+// Mueve continuamente cada 50 ms
+setInterval(movePet, 50);
