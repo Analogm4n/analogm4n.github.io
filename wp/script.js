13a3a570c566efb9016a22b1bd0921a77d4842a5
Jp <160363802+Analogm4n@users.noreply.github.com>
2024-11-06 09:23:21 +0100

    Add files via upload
    wp updated...

---
+++ wp/script.js
@@ -0,0 +1,31 @@
+const pet = document.getElementById('pet');
+
+const petGifRight = '../images/cat_right_sneak1_9fps.gif';  
+const petGifLeft = '../images/cat_left_sneak1_9fps.gif';  
+
+let positionX = window.innerWidth - pet.clientWidth - 20;
+let direction = -1; // 1: derecha, -1: izquierda
+
+// Establece los límites
+const leftLimit = -80; // Límite izquierdo (0 píxeles desde el borde izquierdo)
+const rightLimit = window.innerWidth - pet.clientWidth -20 ; // Límite derecho (0 píxeles desde el borde derecho)
+
+// Función para mover de un lado a otro
+function movePet() {
+    // Cambia la dirección si alcanza un borde de la pantalla
+    if (positionX <= leftLimit) {
+        direction = 1; 
+        pet.src = petGifRight; 
+    } else if (positionX >= rightLimit) {
+        direction = -1; 
+        pet.src = petGifLeft; 
+    }
+
+    // Actualiza la posición en X
+    positionX += 5 * direction;
+    pet.style.transform = `translateX(${positionX}px)`;
+}
+
+// Mueve continuamente cada 50 ms
+setInterval(movePet, 50);
+
